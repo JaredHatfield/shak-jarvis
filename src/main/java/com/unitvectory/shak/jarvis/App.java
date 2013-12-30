2e8716eaa571ad6975690841350ef32d3ee74808
Jared Hatfield <jjhatf02@gmail.com>
2013-12-30 12:41:40 -0500

    Empty Maven project.

---
+++ src/main/java/com/unitvectory/shak/jarvis/App.java
@@ -0,0 +1,18 @@
+package com.unitvectory.shak.jarvis;
+
+/**
+ * Main application.
+ * 
+ */
+public class App {
+
+    /**
+     * Main application.
+     * 
+     * @param args
+     *            the args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
